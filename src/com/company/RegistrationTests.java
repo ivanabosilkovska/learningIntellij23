ce0dff51715f57c347bcb424e8bcd87f9919859e
Ivana <bosilkovska.ivana@gmail.com>
2023-05-22 19:38:09 +0200

    added one registraition test

---
+++ src/com/company/RegistrationTests.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class RegistrationTests {
+
+    public static void main(String[] args) {
+        //adding your automation tests
+
+        System.out.println("added first positive test");
+    }
+}
